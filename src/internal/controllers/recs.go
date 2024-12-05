7dceac1a9ca6b7ccbdb500604efe406c2910aba1
Nikita <tyumin000@gmail.com>
2024-12-05 12:25:31 +0300

    feat: return parsed json

--- src/internal/controllers/recs.go
+++ src/internal/controllers/recs.go
@@ -3,9 +3,11 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log/slog"
+	"net/http"
 )
 
 type RecsController struct {
@@ -32,9 +34,18 @@ func (con *RecsController) Recommend(c *gin.Context) {
 	res, err := con.recommender.Recommend(context.TODO())
 
 	if err != nil {
-		c.JSON(500, fmt.Sprintf("failed to retrieve recommendations: %s. %v", err.Error(), err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("failed to retrieve recommendations: %s. %v", err.Error(), err),
+		})
 		return
 	}
 
-	c.JSON(200, res)
+	var jsonData map[string]interface{}
+	err = json.Unmarshal([]byte(res), &jsonData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse recommendations"})
+		return
+	}
+
+	c.JSON(http.StatusOK, jsonData)
 }
